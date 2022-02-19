792576752f31573a00b067cc49a7a96b19a53028
AnyamahNdidi <theo4felix@gmail.com>
2022-02-19 17:47:41 +0100

    my part of the project

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import DSlider from './Component/DSlider/DSlider';
+import Allsales from './Component/AllSales/Allsales';
+import About from './Component/About/About';
+import Allabout from './Component/About/Allabout';
+
+function App() {
+  return (
+    <div >
+    
+      {/* <DSlider/>
+        <br/>
+        <br/>
+      <Allsales/>
+      <div style={{ height:"600px", width:"100%", backgorindColor:"red"}}>
+
+      </div> */}
+      <Allabout/>
+
+      <div style={{ height:"600px", width:"100%", backgorindColor:"red"}}>
+
+</div>
+    </div>
+  );
+}
+
+export default App;
